5b619d360c319ee9059af53e0aca5ee9c7679603
capocolo <gabrielserna.hce@gmail.com>
2022-04-21 12:21:37 +0200

    aggiunga home

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './home/home.js';
 import MainTemplate from './mainLayout/template/mainTemplate.js';
+import '../css/style.css';
 
 function App() {
   return (
